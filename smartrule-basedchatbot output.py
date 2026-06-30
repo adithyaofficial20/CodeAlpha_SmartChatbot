941d7fca2b5c2d338c461d12256f455f264ed3d7
Adithya A <aadithyaajith2006@gmail.com>
2026-06-30 16:59:43 +0530

    Add files via upload

---
+++ smartrule-basedchatbot output.py
@@ -0,0 +1,63 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============================================== RESTART: D:/smartrule-basedchatbot.py ==============================================
+==================================================
+SMART CHATBOT
+==================================================
+
+Enter Your Name: Adithya
+
+Hello Adithya! Type 'help' to see commands.
+
+Adithya: hello
+
+Bot: Hello Adithya! Nice to see you.
+
+Adithya: joke
+
+Bot: Why do Java developers wear glasses? Because they cannot C#.
+
+Adithya: quote
+
+Bot: Dream big. Start small. Act now.
+
+Adithya: calculator
+Enter First Number: 5
+Enter Second Number: 4
+Operation (+,-,*,/): +
+Result = 9.0
+
+Adithya: calulator
+
+Bot: Sorry, I don't understand that command.
+
+Adithya: about
+
+Bot: I am a Python Rule-Based Chatbot.
+
+Adithya: help
+
+AVAILABLE COMMANDS
+------------------------------
+hello
+time
+date
+calculator
+joke
+quote
+about
+help
+bye
+
+Adithya: time
+
+Bot: Current Time: 04:56 PM
+
+Adithya: date
+
+Bot: Today's Date: 30-06-2026
+
+Adithya: bye
+
+Bot: Goodbye! Have a great day.
